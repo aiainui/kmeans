1c715232780d295c08d5877758c635f28639a60d
aiainui <379879419@qq.com>
2018-02-03 16:48:43 +0800

    Add files via upload

---
+++ isStopWords.py
@@ -0,0 +1,17 @@
+def isStopWords(term):
+    import pickle
+    from tools import File_Interface as FI
+
+    #seg_list = jieba.cut(question, cut_all=False, HMM=True)
+    #senetence = r'吗'
+    ret = []
+    ret = FI.load_pickle('D:/python34/program/tools/stop_words.pkl')
+    #print(ret)
+    if(term in ret):
+        return True
+    else:
+        return False
+
+
+# result = isStopWords()
+# print(result)
